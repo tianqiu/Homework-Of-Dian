3c4244bba4396d32cb32ff66ba07d01b75e6e5f9
apqiutian <406672095@qq.com>
2015-12-13 19:59:37 +0800

    2

--- linux培训/resource/resource/task_scripts/Group3/t.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-region="$(dirname $1)"
-
-mixedregion="$(echo ${region%${region#?}} | tr '[[:lower:]]' '[[:upper:]]')\
-$(echo ${region#?} | tr '[[:upper:]]' '[[:lower:]]')"
-
-echo $mixedregion
